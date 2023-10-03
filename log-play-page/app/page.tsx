19984d7c3e09927fff906a0f0658f2ee44ce19df
Piotr Zajac <ptr.zjc@gmail.com>
2023-10-03 11:33:58 +0200

    wip

---
+++ log-play-page/app/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { GameForm } from '@/app/GameForm';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <p>Hello world</p>
+      <GameForm/>
+    </main>
+  )
+}
