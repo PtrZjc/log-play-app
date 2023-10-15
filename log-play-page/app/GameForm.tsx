fc964ec901fbbeaa6d3749a26353414d62d58242
Piotr Zajac <ptr.zjc@gmail.com>
2023-10-15 23:46:54 +0200

    Work on form

--- log-play-page/app/GameForm.tsx
+++ log-play-page/app/GameForm.tsx
@@ -1,110 +1,151 @@
-import React from 'react';
-import {Button, Input, Form, Select, AutoComplete} from 'antd';
+import React, {useEffect, useState} from 'react';
+import {
+  AutoComplete,
+  Button,
+  Checkbox,
+  Col,
+  DatePicker,
+  Form,
+  Input,
+  Row,
+  Select,
+  Space,
+  Switch, TimePicker,
+} from 'antd';
 import {GAMES, PLAYERS} from "@/app/constants";
+import dayjs from "dayjs";
+import {MinusCircleOutlined, PlusOutlined} from "@ant-design/icons";
+import './globals.css';
 
-const { Option } = Select;
+const {Option} = Select;
 
 const layout = {
-  labelCol: { span: 8 },
-  wrapperCol: { span: 16 },
+  labelCol: {span: 8},
+  wrapperCol: {span: 16},
 };
 
 const tailLayout = {
-  wrapperCol: { offset: 8, span: 16 },
+  wrapperCol: {offset: 8, span: 16},
 };
 
 type GameFormParams = Record<string, string>
 
 export const GameForm: React.FC = () => {
+  
   const [form] = Form.useForm();
-
-  const onGenderChange = (value: any) => {
-    switch (value) {
-      case 'male':
-        form.setFieldsValue({ note: 'Hi, man!' });
-        break;
-      case 'female':
-        form.setFieldsValue({ note: 'Hi, lady!' });
-        break;
-      case 'other':
-        form.setFieldsValue({ note: 'Hi there!' });
-        break;
-      default:
-    }
-  };
-
-  const onFinish = (values: any) => {
-    console.log(values);
-  };
-
-  const onReset = () => {
-    form.resetFields();
-  };
-
-  const onFill = () => {
-    form.setFieldsValue({ note: 'Hello world!', gender: 'male' });
-  };
-
-  const options = [
-    { value: 'Burns Bay Road' },
-    { value: 'Downing Street' },
-    { value: 'Wall Street' },
-  ];
-
+  
+  const [fixedTime, setFixedTime] = useState(false);
+  
+  const onFinish = (values: any) => console.log(JSON.stringify(values, null, 2));
+  
   const insensitiveCaseIncludes = (input: string, option: any) => {
     const candidate = option.value.toLowerCase();
     const search = input.toLowerCase();
     return candidate.includes(search);
   };
-
+  
+  
   return (
-      <Form
-          {...layout}
+    <Row justify={"center"}>
+      <Col span={9}>
+        <Form
+          initialValues={
+            {
+              gameDate: dayjs(),
+              playerResults: [
+                {playerName: PLAYERS[0]},
+                {playerName: PLAYERS[1]},
+              ]
+            }}
           form={form}
-          name="control-hooks"
           onFinish={onFinish}
-//          style={{ maxWidth: 600 }}
-      >
-        <Form.Item name="gameName" label="Game" rules={[{ required: true }]}>
-          <AutoComplete
-              options={GAMES.map(player => ({ value: player }))}
-              filterOption={insensitiveCaseIncludes}
-          />
-        </Form.Item>
-        <Form.Item name="gender" label="Gender" rules={[{ required: true }]}>
-          <Select
-              placeholder="Select a option and change input text above"
-              onChange={onGenderChange}
-              allowClear
-          >
-            <Option value="male">male</Option>
-            <Option value="female">female</Option>
-            <Option value="other">other</Option>
-          </Select>
-        </Form.Item>
-        <Form.Item
-            noStyle
-            shouldUpdate={(prevValues, currentValues) => prevValues.gender !== currentValues.gender}
         >
-          {({ getFieldValue }) =>
-              getFieldValue('gender') === 'other' ? (
-                  <Form.Item name="customizeGender" label="Customize Gender" rules={[{ required: true }]}>
-                    <Input />
-                  </Form.Item>
-              ) : null
-          }
-        </Form.Item>
-        <Form.Item {...tailLayout}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-          <Button htmlType="button" onClick={onReset}>
-            Reset
-          </Button>
-          <Button type="link" htmlType="button" onClick={onFill}>
-            Fill form
-          </Button>
-        </Form.Item>
-      </Form>
+          <Form.Item name="gameName" label="Game" rules={[{required: true}]}>
+            <AutoComplete
+              options={GAMES.map(game => ({value: game}))}
+              filterOption={insensitiveCaseIncludes}
+            />
+          </Form.Item>
+          <Form.Item name="gameDate" label="Game Date">
+            <DatePicker
+              disabledDate={date => date.isAfter(dayjs())}
+            />
+          </Form.Item>
+          <div className={"flex justify-between items-center"}>
+            <Switch
+              className={"-mt-6"}
+              checkedChildren="Fixed Time"
+              unCheckedChildren="Time Range"
+              onChange={setFixedTime}
+            />
+            <div className={"flex flex-grow justify-center"}>
+              
+              {!fixedTime ?
+                <Form.Item name="rangeTime">
+                  <TimePicker.RangePicker
+                    format="HH:mm"
+                    minuteStep={5}
+                  />
+                </Form.Item> :
+                <Form.Item name="fixedTime">
+                  <TimePicker
+                    format="HH:mm"
+                    showNow={false}
+                    minuteStep={5}
+                  />
+                </Form.Item>
+              }
+            </div>
+          </div>
+          <Form.List name="playerResults">
+            {(fields, {add, remove}) => (
+              <>
+                {fields.map(({key, name, ...restField}) => (
+                  <Space key={key} align="baseline">
+                    <Form.Item
+                      {...restField}
+                      name={[name, 'playerName']}
+                      rules={[{required: true, message: 'Missing player name'}]}
+                    >
+                      <AutoComplete
+                        style={{width: 200}}
+                        placeholder={"Player Name"}
+                        options={PLAYERS.map(player => ({value: player}))}
+                        filterOption={insensitiveCaseIncludes}
+                      />
+                    </Form.Item>
+                    <Form.Item
+                      {...restField}
+                      name={[name, 'playerScore']}
+                      rules={[{required: true, message: 'Missing last name'}]}
+                    >
+                      <Input type="number" placeholder="Score"/>
+                    </Form.Item>
+                    <Form.Item
+                      {...restField}
+                      name={[name, 'isWinner']}
+                    >
+                      <Checkbox>Won</Checkbox>
+                    </Form.Item>
+                    <MinusCircleOutlined onClick={() => remove(name)}/>
+                  </Space>
+                ))}
+                <Form.Item>
+                  <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined/>}>
+                    Add field
+                  </Button>
+                </Form.Item>
+              </>
+            )}
+          </Form.List>
+          
+          <Form.Item {...tailLayout}>
+            <Button type="primary" htmlType="submit">
+              Add
+            </Button>
+          </Form.Item>
+        </Form>
+      </Col>
+    </Row>
   );
 };
--- log-play-page/app/page.tsx
+++ log-play-page/app/page.tsx
@@ -5,7 +5,6 @@ import { GameForm } from '@/app/GameForm';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <p>Hello world</p>
       <GameForm/>
     </main>
   )
